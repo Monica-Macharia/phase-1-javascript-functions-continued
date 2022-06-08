cf8be9f192873d277fedd38091dd644a173ecbf1
Monica Nyaguthii Macharia <monicamacharia7@gmail.com>
2022-06-08 12:31:40 +0300

    commit tree da124c023a5988d28918c526e7ec8abeceffbfcc

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // code your solution here
+function saturdayFun(hobby = "roller-skate"){
+    return `This Saturday, I want to ${hobby}!`;
+}
+    let mondayWork = function (task = 'go to the office'){
+    return `This Monday, I will ${task}.`;
+};
+
+const wrapAdjective = function(all = "*"){
+    return function inner (say = "good"){
+    return `You are ${all}${say}${all}!`
+ }
+}
+
